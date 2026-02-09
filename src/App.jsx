090adefe5af0df9ce6e5435e2d54178f6ee942b2
bharath yallala <yallalabharath00@gmail.com>
2026-02-09 21:17:53 +0530

    changes in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import Alerts from "./pages/Alerts";
 import Logs from "./pages/Logs";
 import Reports from "./pages/Reports";
 import Settings from "./pages/Settings";
-import ProtectedRoute from "./components/ProtectedRoute";
+import ProtectedRoutes from "./components/ProtectedRoutes";
+import Alert from "./pages/Alert";
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/login" element={<Login />} />
 
-      <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-      <Route path="/alerts" element={<ProtectedRoute><Alerts /></ProtectedRoute>} />
-      <Route path="/logs" element={<ProtectedRoute><Logs /></ProtectedRoute>} />
-      <Route path="/reports" element={<ProtectedRoute><Reports /></ProtectedRoute>} />
-      <Route path="/settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} />
+      <Route path="/dashboard" element={<ProtectedRoutes><Dashboard /></ProtectedRoutes>} />
+      <Route path="/alerts" element={<ProtectedRoutes><Alert /></ProtectedRoutes>} />
+      <Route path="/logs" element={<ProtectedRoutes><Logs /></ProtectedRoutes>} />
+      <Route path="/reports" element={<ProtectedRoutes><Reports /></ProtectedRoutes>} />
+      <Route path="/Settings" element={<ProtectedRoutes><Settings /></ProtectedRoutes>} />
     </Routes>
   </BrowserRouter>
 );
