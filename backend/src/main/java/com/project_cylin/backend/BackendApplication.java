c1614537198b515feb8035e8e733b977e2b17ead
gurucharans0705-star <gurucharan.s0705@gmail.com>
2026-02-10 11:15:15 +0530

    main structure and application.properties are changed

--- backend/src/main/java/com/project_cylin/backend/BackendApplication.java
+++ backend/src/main/java/com/project_cylin/backend/BackendApplication.java
@@ -1,13 +1,12 @@
-package com.project_cylin.backend;
+package com.cybersecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class CyberSecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CyberSecurityApplication.class, args);
+    }
 }
